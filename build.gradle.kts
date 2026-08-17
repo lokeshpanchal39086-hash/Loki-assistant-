19bf4ae7e8a1cb8af854155e0049dbc586567e4c
lokeshpanchal39086-hash <lokeshpanchal39086@gmail.com>
2026-08-17 15:57:47 +0530

    Update plugin versions in build.gradle.kts
    Updated Android and Kotlin plugin versions in build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.loki.assistant"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.loki.assistant"
-        minSdk = 23
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
+    id("com.android.application") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
